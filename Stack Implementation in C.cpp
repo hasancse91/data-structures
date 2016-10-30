b1965373793951e619e85e0f9bf54a767cc76686
hasan_cse91 <hasan_cse91@yahoo.com>
2016-10-31 01:42:25 +0600

    stack code added

---
+++ Stack Implementation in C.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#define stackSize 100
+
+int myStack[stackSize], top = -1;
+
+void push(int value)
+{
+    if(top>=stackSize)
+        printf("Stack is full!\n");
+    else
+        myStack[++top] = value;
+}
+
+void peek()
+{
+    if(top<0)
+        printf("Stack Underflow!\n");
+    else
+        printf("%d\n", myStack[top]);
+}
+
+void pop()
+{
+    if(top<0)
+        printf("Stack underflow!\n");
+    else
+        top--;
+}
+
+void displayStack()
+{
+    printf("Print the full stack from TOP to BOTTOM:\n");
+    for(int i = top; i>=0; i--)
+        printf("%d\n",myStack[i]);
+}
+
+int main()
+{
+    push(5);
+    push(90);
+    push(12);
+
+    displayStack();
+
+    pop();
+
+    displayStack();
+
+    printf("\nTop value of this Stack:   ");
+    peek();
+
+
+    return 0;
+}
